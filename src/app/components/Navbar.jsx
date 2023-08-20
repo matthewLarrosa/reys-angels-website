a75a28e0b5f5779ee4489ef6cac0e592ae6c6790
matthewLarrosa <matthewterra16@gmail.com>
2023-08-20 19:21:21 -0400

    navbar created

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,20 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import logo from '../assets/reysAngelsLogo.svg'
+import { HiMenu } from 'react-icons/hi';
 
 function Navbar() {
   return (
-    <div className='flex h-10 w-screen'>
-      <div className=''>
-        <Image src={logo} alt="Rey's Angels Logo"/>
+    <div className='flex justify-between items-center h-10 w-screen'>
+      <div className='p-2'>
+        <Image className='h-auto w-32' src={logo} alt="Rey's Angels Logo" />
       </div>
-      <div className=''>
-        nav button
+      <div className='p-1 mt-1'>
+        <HiMenu className='h-7 w-7'>
+          <button></button>
+        </HiMenu>
       </div>
     </div>
   )
