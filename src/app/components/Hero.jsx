50a8292a32c2f1142c015965170e77f688f8200d
Matthew Larrosa <Matthewterra16@gmail.com>
2023-08-21 16:58:28 -0400

    Hero Section Styling

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,9 +1,16 @@
+"use client";
+
 import React from 'react'
+import Image from "next/image";
+import shirt from "../assets/shirt.png";
+
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="w-screen h-128 bg-slate-400 relative">
+      <Image className='h-auto w-auto mt-10' src={shirt} alt='shirt'></Image>
+    </div>
+  );
 }
 
 export default Hero
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,26 +8,23 @@ import { motion, useAnimation } from "framer-motion";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  
-  const controls = useAnimation();
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    controls.start({ y: isMenuOpen ? -50 : 0 }); // Move the button up when opening, down when closing
   };
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-10 w-screen backdrop-blur-md bg-opacity-90">
+    <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-14 w-screen backdrop-blur-md bg-white bg-opacity-90">
       <div className="p-2">
-        <Image className="h-auto w-32" src={logo} alt="Rey's Angels Logo" />
+        <Image className="h-auto w-40 mt-2 ml-2" src={logo} alt="Rey's Angels Logo" />
       </div>
       <div className="p-1 mt-1">
-        <motion.button
+        <button
           onClick={toggleMenu}
-          className="h-7 w-14"
-          animate={controls}
+          className="h-10 w-10 mt-1 mr-3"
         >
-          <HiMenu className="h-7 w-7" />
-        </motion.button>
+          <HiMenu className="h-10 w-10 mt-1 mr-2" />
+        </button>
         {isMenuOpen && (
           <div className="absolute top-8 right-0 mt-2 p-4 bg-white shadow-md">
             <ul className="space-y-4 text-lg">
