a5df84a77e35efee2ced5de0ec562e26348d1618
matthewLarrosa <matthewterra16@gmail.com>
2023-08-20 20:01:39 -0400

    styling menu buttons, framer motion added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,11 +7,11 @@ import { HiMenu } from 'react-icons/hi';
 
 function Navbar() {
 
-  function handleClick(event) { 
-    
-    console.log("hello") 
-    
-  } 
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
   return (
     <div className='fixed top-0 left-0 right-0 z-50 flex justify-between items-center h-10 w-screen backdrop-blur-md bg-opacity-90'>
@@ -19,9 +19,27 @@ function Navbar() {
         <Image className='h-auto w-32' src={logo} alt="Rey's Angels Logo" />
       </div>
       <div className='p-1 mt-1'>
-          <button onClick={handleClick}>
+          <button onClick={toggleMenu}>
             <HiMenu className='h-7 w-7'/>
             </button>
+            {isMenuOpen && (
+          <div className='absolute top-8 right-0 mt-2 p-4 bg-white shadow-md'>
+            <ul className='space-y-4 text-lg'>
+              <li className='w-32'>
+                <a href='#home'>Home</a>
+              </li>
+              <li>
+                <a href='#about'>About</a>
+              </li>
+              <li>
+                <a href='#contact'>Contact</a>
+              </li>
+              <li>
+                <a href='#pricing'>Pricing</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   )
