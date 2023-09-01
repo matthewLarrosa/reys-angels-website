e81c8cee37d20820ded68ea0de4c57506519615e
Matthew Larrosa <Matthewterra16@gmail.com>
2023-08-31 20:51:53 -0400

    hero moblie implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,30 @@
+import ideas from "../assets/ideas.svg";
+import vinyl from "../assets/vinyl.svg";
+import shirt from "../assets/shirtPortion.png";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="relative">
+      <div className="absolute top-32 -z-10">
+        <img src={shirt} alt="shirt" />
+      </div>
+      <div className="flex justify-center flex-col h-auto w-auto mt-32">
+        <div className="flex justify-center flex-col text-center">
+          <img src={ideas} alt="hero-text" className="h-16 mt-24" />
+          <img src={vinyl} alt="hero-text" className="h-16" />
+          <div className="font-robo font-bold text-xl">
+            Based in Miami, Florida
+          </div>
+          <div className="font-robo font-bold text-xl">since 2015</div>
+          <div className="mt-2">
+            <button className="h-9 w-36 bg-black font-robo text-white rounded-3xl">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
