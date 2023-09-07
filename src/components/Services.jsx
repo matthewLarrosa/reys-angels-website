56dc058ea7817d983f6b73c4320394d028c5e6c0
Matthew Larrosa <Matthewterra16@gmail.com>
2023-09-07 15:42:57 -0400

    Sevices page info added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,19 +2,18 @@
 function Services() {
   return (
     <div className='relative flex flex-col backdrop-blur-md h-96 w-screen'>
-      <div className="text-4xl font-lex font-bold text-center mt-6">Our Services</div>
-      <div className="text-xl font-robo text-center mt-1">What can we do you?</div>
-      
-      <div>
-        <div className="text-lg"><div className="text-xl font-robo">Versatility</div>Shirts,pants,hats,bottles, even sports equipment. If you can wear it and or hold it we can put vinyl on it.</div>
+      <div className="text-3xl font-lex font-bold text-center mt-6">Our Services</div>
+      <div className="text-lg font-robo text-center mt-1 text-red-500">What can we do you?</div>
+      <div className="text-center mt-9">
+        <div className="text-xl font-robo">Versatility</div>
+        <div className="text-lg p-4 m-2 border rounded-xl">Shirts, pants, hats, bottles, even sports equipment. If you can wear it and or hold it we can put vinyl on it.</div>
       </div>
 
-      <div>
+      <div className="text-center mt-2">
         <div className="text-xl font-robo">Order Quantity</div>
-        <div className="text-lg">Whether its a single item or for a group, no order is too big.</div>
+        <div className="text-lg p-4 m-2 border rounded-xl">Whether its a single item or for a group, no order is too big.</div>
       </div>
     
-
     </div>
   )
 }
